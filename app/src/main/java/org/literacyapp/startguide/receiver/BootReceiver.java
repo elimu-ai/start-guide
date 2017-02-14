ab0d344a3c833e833642dcd26b3a641dd9ac2459
gsc <gsc.mobile.dev@gmail.com>
2017-02-14 23:59:20 +0100

    Added BootReceiver to start the aplication

---
+++ app/src/main/java/org/literacyapp/startguide/receiver/BootReceiver.java
@@ -0,0 +1,23 @@
+package org.literacyapp.startguide.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import org.literacyapp.startguide.content.swipe.SwipeUpDownActivity;
+
+/**
+ * Created by GSC on 13/02/2017.
+ */
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent bootIntent = new Intent(context, SwipeUpDownActivity.class);
+            bootIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(bootIntent);
+        }
+    }
+}
