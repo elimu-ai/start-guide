90994752c73032f2100a7db25ac6dd644d2a2e3a
gsc <gsc.mobile.dev@gmail.com>
2017-04-04 23:34:06 +0200

    Added gestures: single tap, double tap, press and hold

--- handgesture/src/main/java/org/literacyapp/handgesture/Gestures.java
+++ handgesture/src/main/java/org/literacyapp/handgesture/Gestures.java
@@ -8,23 +8,37 @@ public class Gestures {
     public static HandGesture MOVE_DOWN = HandGesture.MOVE_DOWN;
     public static HandGesture MOVE_LEFT = HandGesture.MOVE_LEFT;
     public static HandGesture MOVE_RIGHT = HandGesture.MOVE_RIGHT;
-    public static HandGesture ONE_TOUCH = HandGesture.ONE_TOUCH;
+    public static HandGesture SINGLE_TAP = HandGesture.SINGLE_TAP;
+    public static HandGesture DOUBLE_TAP = HandGesture.DOUBLE_TAP;
+    public static HandGesture PRESS_AND_HOLD = HandGesture.PRESS_AND_HOLD;
 
-    public enum HandGesture {
+    protected enum HandGesture {
         MOVE_UP(R.anim.slide_up),
         MOVE_DOWN(R.anim.slide_down),
         MOVE_LEFT(R.anim.slide_left),
         MOVE_RIGHT(R.anim.slide_right),
-        ONE_TOUCH(0);
+        SINGLE_TAP(0, false),
+        DOUBLE_TAP(0, false),
+        PRESS_AND_HOLD(0, false);
 
         private int idAnim;
+        private boolean translation;
 
         HandGesture(int idAnim) {
+            this(idAnim, true);
+        }
+
+        HandGesture(int idAnim, boolean translation) {
             this.idAnim = idAnim;
+            this.translation = translation;
         }
 
         public int getAnimationResource() {
             return idAnim;
+        }
+
+        public boolean isTranslation() {
+            return translation;
         }
     }
 
--- handgesture/src/main/java/org/literacyapp/handgesture/HandView.java
+++ handgesture/src/main/java/org/literacyapp/handgesture/HandView.java
@@ -2,20 +2,34 @@ package org.literacyapp.handgesture;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
 import android.widget.RelativeLayout;
+
+import org.literacyapp.handgesture.Gestures.HandGesture;
+
+import static org.literacyapp.handgesture.Gestures.DOUBLE_TAP;
+import static org.literacyapp.handgesture.Gestures.PRESS_AND_HOLD;
+import static org.literacyapp.handgesture.Gestures.SINGLE_TAP;
 
 /**
  */
 public class HandView extends RelativeLayout implements AnimationHelper.TouchListener {
 
+    private static final long SCALE_DURATION = 500;
+
     private View mView;
+    private View mTouchScreenView;
 
     private AnimationHelper mAnimationHelper;
+    private HandGesture handGesture;
 
     private int mAnimationType;
     private boolean mHideOnTouch = true;
@@ -37,6 +51,8 @@ public class HandView extends RelativeLayout implements AnimationHelper.TouchLis
 
     private void init(Context context, AttributeSet attrs) {
         mView = inflate(context, R.layout.hand_layout, this);
+
+        mTouchScreenView = mView.findViewById(R.id.circles);
 
         if (attrs != null) {
             // Attribute initialization
@@ -70,28 +86,24 @@ public class HandView extends RelativeLayout implements AnimationHelper.TouchLis
 
     public void startAnimation() {
         if (mAnimationType >= 0) {
-            startAnimation(Gestures.getAnimationResource(mAnimationType));
+            startAnimation(HandGesture.values()[mAnimationType].getAnimationResource());
         } else {
             Log.d(getClass().getName(), "Animation type is not stablished");
         }
     }
 
-    public void startAnimation(Gestures.HandGesture handAnimation) {
-        startAnimation(handAnimation.getAnimationResource());
+    public void startAnimation(HandGesture handAnimation) {
+        if (handAnimation.isTranslation()) {
+            startAnimation(handAnimation.getAnimationResource());
+        } else {
+            startGesture(handAnimation);
+        }
     }
 
     public void startAnimation(int idAnimResource) {
         mAnimationHelper = new AnimationHelper(getContext(), idAnimResource, this);
         mAnimationHelper.setRepeatMode(mRepeatAnimation);
         mAnimationHelper.animateView(this, mAnimationDelay*1000);
-    }
-
-    public void startTouch() {
-        mView.findViewById(R.id.circles).setVisibility(VISIBLE);
-    }
-
-    public void endTouch() {
-        mView.findViewById(R.id.circles).setVisibility(GONE);
     }
 
     //region TouchListener
@@ -105,4 +117,84 @@ public class HandView extends RelativeLayout implements AnimationHelper.TouchLis
         endTouch();
     }
     //endregion
+
+    private void startTouch() {
+        mTouchScreenView.setVisibility(VISIBLE);
+    }
+
+    private void endTouch() {
+        mTouchScreenView.setVisibility(GONE);
+    }
+
+    private void startGesture(HandGesture handGesture) {
+        this.handGesture = handGesture;
+        if (handGesture.equals(SINGLE_TAP)) {
+            startOneTouchAnimation();
+        } else if (handGesture.equals(DOUBLE_TAP)) {
+            startDoubleTouchAnimation();
+        } else if (handGesture.equals(PRESS_AND_HOLD)) {
+            startPressAndHoldAnimation();
+        }
+    }
+
+    private void startOneTouchAnimation() {
+        startTouchAnimation(1000, false);
+    }
+
+    private void startDoubleTouchAnimation() {
+        startTouchAnimation(200, true);
+    }
+
+    private void startPressAndHoldAnimation() {
+        startTouchAnimation(2000, false);
+    }
+
+    private void startTouchAnimation(final long pressTime, boolean repeat) {
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.setFillAfter(true);
+
+        ScaleAnimation scale = new ScaleAnimation(1f, 0.9f, 1f, 0.9f);
+        scale.setStartOffset(1000);
+        scale.setDuration(SCALE_DURATION);
+        scale.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mTouchScreenView.setVisibility(VISIBLE);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTouchScreenView.setVisibility(GONE);
+                    }
+                }, pressTime);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+
+        animationSet.addAnimation(scale);
+
+        ScaleAnimation scale2 = new ScaleAnimation(0.9f, 1f, 0.9f, 1f);
+        scale2.setStartOffset(1000+SCALE_DURATION+pressTime);
+        scale2.setDuration(SCALE_DURATION);
+        animationSet.addAnimation(scale2);
+
+        mView.setAnimation(animationSet);
+
+        if (repeat)
+            animationSet.setRepeatCount(2);
+
+        animationSet.start();
+    }
+
+    @Override
+    protected void onAnimationEnd() {
+        super.onAnimationEnd();
+        if (handGesture != null) {
+            startGesture(handGesture);
+        }
+    }
 }
