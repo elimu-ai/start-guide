2ff7279635133b8c00d1f2ed74751dde8e86f09f
gsc <gsc.mobile.dev@gmail.com>
2017-04-19 00:11:25 +0200

    Fix bug delay for repeat animation

--- handgesture/src/main/java/org/literacyapp/handgesture/Constants.java
+++ handgesture/src/main/java/org/literacyapp/handgesture/Constants.java
@@ -6,9 +6,9 @@ public class Constants {
 
     public static final int MILLISECONDS = 1000;
 
-    public static final long DEFAULT_ANIMATION_DELAY = 1000;
+    public static final long DEFAULT_ANIMATION_DELAY = 1; //seconds
 
-    public static final long SCALE_DURATION = 500;
+    public static final long SCALE_DURATION = 500; //milliseconds
 
     public static final float SCALE_FACTOR = 0.9f;
 }
