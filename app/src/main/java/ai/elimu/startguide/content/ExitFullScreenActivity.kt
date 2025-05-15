83517f7eda4bff663a71bee41e0c53581824ddff
tuancoltech <tuancoltech@gmail.com>
2025-05-15 22:38:13 +0700

    Convert ExitFullScreenActivity to Kotlin

--- app/src/main/java/ai/elimu/startguide/content/ExitFullScreenActivity.kt
+++ app/src/main/java/ai/elimu/startguide/content/ExitFullScreenActivity.kt
@@ -1,165 +1,163 @@
-package ai.elimu.startguide.content;
+package ai.elimu.startguide.content
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
+import ai.elimu.handgesture.HandView
+import ai.elimu.handgesture.HandViewListener
+import ai.elimu.startguide.R
+import ai.elimu.startguide.util.MediaPlayerHelper
+import ai.elimu.startguide.util.MediaPlayerHelper.MediaPlayerListener
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnSystemUiVisibilityChangeListener
 
-import ai.elimu.handgesture.HandView;
-import ai.elimu.handgesture.HandViewListener;
-import ai.elimu.startguide.R;
-import ai.elimu.startguide.util.MediaPlayerHelper;
+class ExitFullScreenActivity : Activity() {
+    private var mHandView: HandView? = null
+    private var mHandViewBottom: HandView? = null
+    private var mBottomBar: View? = null
+    private var mDecorView: View? = null
 
-public class ExitFullScreenActivity extends Activity {
+    private var mAnimationCompleted = false
 
-    private static final long SHORT_PLAYER_DELAY = 500;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exit_full_screen)
 
-    private HandView mHandView;
-    private HandView mHandViewBottom;
-    private View mBottomBar;
-    private View mDecorView;
+        mHandView = findViewById<View?>(R.id.hand_view) as HandView
+        mHandViewBottom = findViewById<View?>(R.id.hand_view_bottom) as HandView
+        mBottomBar = findViewById<View>(R.id.bottom_bar)
 
-    private boolean mAnimationCompleted = false;
+        mDecorView = getWindow().getDecorView()
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exit_full_screen);
-
-        mHandView = (HandView) findViewById(R.id.hand_view);
-        mHandViewBottom = (HandView) findViewById(R.id.hand_view_bottom);
-        mBottomBar = findViewById(R.id.bottom_bar);
-
-        mDecorView = getWindow().getDecorView();
-
-        hideSystemBars();
-        playExitFullScreen();
+        hideSystemBars()
+        playExitFullScreen()
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mDecorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
+    override fun onResume() {
+        super.onResume()
+        mDecorView!!.setOnSystemUiVisibilityChangeListener(object :
+            OnSystemUiVisibilityChangeListener {
+            override fun onSystemUiVisibilityChange(visibility: Int) {
                 // System bars will only be "visible" if none of the
                 // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
-                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                if ((visibility and View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
                     // The system bars are visible.
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            hideSystemBars();
-//                            onGestureDetected();
+                    Handler().postDelayed(object : Runnable {
+                        override fun run() {
+                            hideSystemBars()
+                            //                            onGestureDetected();
                         }
-                    }, 3000);
+                    }, 3000)
                 } else {
                     // The system bars are NOT visible.
-                    hideSystemBars();
+                    hideSystemBars()
                 }
             }
-        });
+        })
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
         if (mAnimationCompleted) {
-            mHandView.onTouchEvent(event);
-            mHandViewBottom.onTouchEvent(event);
-            mBottomBar.setVisibility(View.GONE);
+            mHandView!!.onTouchEvent(event)
+            mHandViewBottom!!.onTouchEvent(event)
+            mBottomBar!!.setVisibility(View.GONE)
         }
-        return false;
+        return false
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        onGestureDetected();
+    override fun onBackPressed() {
+        super.onBackPressed()
+        onGestureDetected()
     }
 
-    private void hideSystemBars() {
+    private fun hideSystemBars() {
         // Set the content to appear under the system bars so that the content
         // doesn't resize when the system bars hide and show.
-        mDecorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        mDecorView!!.setSystemUiVisibility(
+            (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE)
+        )
     }
 
-    private void playExitFullScreen() {
+    private fun playExitFullScreen() {
         // TODO: 04/05/2017 audios en/sw Slide down from outside the screen
-        MediaPlayerHelper.playWithDelay(this, R.raw.slide_down_from_outside_the_screen, new MediaPlayerHelper.MediaPlayerListener() {
-            @Override
-            public void onCompletion() {
-                showExitFullScreenAnimation();
-            }
-        });
+        MediaPlayerHelper.playWithDelay(
+            this,
+            R.raw.slide_down_from_outside_the_screen,
+            object : MediaPlayerListener {
+                override fun onCompletion() {
+                    showExitFullScreenAnimation()
+                }
+            })
     }
 
     /**
      * Hand animation to explain exit full screen
      */
-    private void showExitFullScreenAnimation() {
-        mHandView.setVisibility(View.VISIBLE);
-        mHandView.setHandViewListener(new HandViewListener() {
-            @Override
-            public void onHandAnimationEnd() {
-                mHandView.stopAnimation();
-                mBottomBar.setVisibility(View.VISIBLE);
+    private fun showExitFullScreenAnimation() {
+        mHandView!!.setVisibility(View.VISIBLE)
+        mHandView!!.setHandViewListener(object : HandViewListener {
+            override fun onHandAnimationEnd() {
+                mHandView!!.stopAnimation()
+                mBottomBar!!.setVisibility(View.VISIBLE)
 
-                playTouchTheArrow();
+                playTouchTheArrow()
             }
-        });
-        mHandView.startAnimation();
+        })
+        mHandView!!.startAnimation()
     }
 
-    private void playTouchTheArrow() {
-        MediaPlayerHelper.playWithDelay(this, R.raw.and_touch_the_arrow_below, SHORT_PLAYER_DELAY,
-                new MediaPlayerHelper.MediaPlayerListener() {
-                    @Override
-                    public void onCompletion() {
-                        showBottomAnimation();
+    private fun playTouchTheArrow() {
+        MediaPlayerHelper.playWithDelay(
+            this, R.raw.and_touch_the_arrow_below, SHORT_PLAYER_DELAY,
+            object : MediaPlayerListener {
+                override fun onCompletion() {
+                    showBottomAnimation()
                 }
-        });
+            })
     }
 
-    private void showBottomAnimation() {
-        mHandViewBottom.setVisibility(View.VISIBLE);
-        mHandViewBottom.startAnimation();
-        mHandViewBottom.setHandViewListener(new HandViewListener() {
-            @Override
-            public void onHandAnimationEnd() {
-                mBottomBar.setVisibility(View.GONE);
-                mHandViewBottom.setVisibility(View.GONE);
+    private fun showBottomAnimation() {
+        mHandViewBottom!!.setVisibility(View.VISIBLE)
+        mHandViewBottom!!.startAnimation()
+        mHandViewBottom!!.setHandViewListener(object : HandViewListener {
+            override fun onHandAnimationEnd() {
+                mBottomBar!!.setVisibility(View.GONE)
+                mHandViewBottom!!.setVisibility(View.GONE)
 
-                mAnimationCompleted = true;
+                mAnimationCompleted = true
 
-                mHandView.setVisibility(View.VISIBLE);
-                mHandView.setHideOnTouch(false);
-                playExitFullScreen();
+                mHandView!!.setVisibility(View.VISIBLE)
+                mHandView!!.setHideOnTouch(false)
+                playExitFullScreen()
             }
-        });
+        })
     }
 
-    private void onGestureDetected() {
+    private fun onGestureDetected() {
         if (mAnimationCompleted) {
-            mHandView.stopAnimation();
-            mHandView.setVisibility(View.GONE);
-            mHandViewBottom.stopAnimation();
-            mHandViewBottom.setVisibility(View.GONE);
-            mBottomBar.setVisibility(View.GONE);
-            navigateToFinal();
+            mHandView!!.stopAnimation()
+            mHandView!!.setVisibility(View.GONE)
+            mHandViewBottom!!.stopAnimation()
+            mHandViewBottom!!.setVisibility(View.GONE)
+            mBottomBar!!.setVisibility(View.GONE)
+            navigateToFinal()
         }
     }
 
-    private void navigateToFinal() {
-        finish();
-        Intent intent = new Intent(this, FinalActivity.class);
-        startActivity(intent);
+    private fun navigateToFinal() {
+        finish()
+        val intent = Intent(this, FinalActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        private const val SHORT_PLAYER_DELAY: Long = 500
     }
 }
