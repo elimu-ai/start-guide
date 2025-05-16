dcc7b4a95376fb1c018efb9f66c62e1af898505b
tuancoltech <tuancoltech@gmail.com>
2025-05-16 12:44:54 +0700

    Use viewBinding in TestGestureActivity

--- app/src/main/java/ai/elimu/startguide/TestGestureActivity.kt
+++ app/src/main/java/ai/elimu/startguide/TestGestureActivity.kt
@@ -1,26 +1,25 @@
 package ai.elimu.startguide
 
 import ai.elimu.handgesture.Gestures
-import ai.elimu.handgesture.HandView
+import ai.elimu.startguide.databinding.ActivityTestGestureBinding
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
 
 class TestGestureActivity : Activity() {
+
+    private lateinit var binding: ActivityTestGestureBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test_gesture)
+        binding = ActivityTestGestureBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val mSingleTap = findViewById<View?>(R.id.single_tap) as HandView
-        mSingleTap.startAnimation(Gestures.SINGLE_TAP)
+        binding.singleTap.startAnimation(Gestures.SINGLE_TAP)
 
-        val mDoubleTap = findViewById<View?>(R.id.double_tap) as HandView
-        mDoubleTap.startAnimation(Gestures.DOUBLE_TAP)
+        binding.doubleTap.startAnimation(Gestures.DOUBLE_TAP)
 
-        val mPressAndHold = findViewById<View?>(R.id.press_and_hold) as HandView
-        mPressAndHold.startAnimation(Gestures.PRESS_AND_HOLD)
+        binding.pressAndHold.startAnimation(Gestures.PRESS_AND_HOLD)
 
-        val mTranslation = findViewById<View?>(R.id.translation) as HandView
-        mTranslation.startAnimation()
+        binding.translation.startAnimation()
     }
 }
